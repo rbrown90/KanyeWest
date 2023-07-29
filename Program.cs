9724cd3321f024a09df8719221fb482132e82280
Rakin Brown <rakinbrown38@gmail.com>
2023-07-29 13:48:46 -0500

    Kanye and Ron api

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeandRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+        } 
+    }
+}
